29f1fbe00bd2c37c0db21a721dc8d1d52afd2bf7
OscarDeLeon19 <oscar99deleon19@gmail.com>
2025-05-21 15:35:13 -0600

    prueba

--- codeChallenge1/src/main/java/main/Main.java
+++ codeChallenge1/src/main/java/main/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Nada");
         System.out.println("Ingresa la primera palabra: ");
         Scanner scanner = new Scanner(System.in);
         String palabra1 = "";
